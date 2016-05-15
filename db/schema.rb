7fd4d1e8661e14d960692425f0c8098a7cc11d91
Kevin Collette <kevcollette@gmail.com>
2016-05-15 16:00:18 -0600

    Add postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160514224745) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "words", force: :cascade do |t|
     t.string  "name"
     t.string  "part_of_speech"
